9ad1edb5343e1986838275374ff97fc8fc5348b0
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2017-04-27 11:12:51 +0200

    fix typos

--- source/ftrack_location_compatibility/plugin.py
+++ source/ftrack_location_compatibility/plugin.py
@@ -195,12 +195,12 @@ class ProxyLegacyLocationMixin(object):
 
 def register_locations(session):
     '''Register proxy locations.'''
-    for location in session.query('select name from Location')
+    for location in session.query('select name from Location'):
 
         '''
         Since the implementation in the new API does not
         translate paths between operating systems using ftrack disks
-        we mixin the proxy location with all the locatino which requires such a feature.
+        we mixin the proxy location with all the location which requires such a feature.
         '''
 
         if (
